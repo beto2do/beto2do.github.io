953bfe4c1ea9662ab71b0257d0650d7ad8cf4800
Humberto <humberto2do@gmail.com>
2024-07-06 22:23:37 -0700

    add initial personal website

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Page: FC = () => {
+    return (
+        <div>
+            <h1>Welcome to articles Page</h1>
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Page: FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Resume Page</h1>
+        </div>
+    );
+};
+
+export default Page;
